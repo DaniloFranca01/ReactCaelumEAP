8c3d615f9e6b17bc2d0294c6c63018b46a7c604f
Danilo Franca <dlf3@cin.ufpe.br>
2019-11-14 04:10:38 -0300

    teste

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, function (req, res) {
   res.sendFile(__dirname + '/dist/index.html');
 })
-app.use(express.static('static'));
-app.get('*', (req, res) => res.sendFile(path.resolve('static', 'index.html')));
+
 app.listen(port);
 
 console.log("server started");
