e10e37e32b4ebf8b2a1cf715838e7deed4f4b24f
Danilo Franca <dlf3@cin.ufpe.br>
2019-11-14 03:34:31 -0300

    static

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
-app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
-})
+app.use(express.static('static'));
+app.get('*', (req, res) => res.sendFile(path.resolve('static', 'index.html')));
 app.listen(port);
 
 console.log("server started");
