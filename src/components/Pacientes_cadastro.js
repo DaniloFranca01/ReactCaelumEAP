0091539451333491e682fa81e2beb30a2fbfe6c4
gstadtler <ghs2@cin.ufpe.br>
2019-11-19 01:15:17 -0300

    atualizações no modulo de pacientes e estilizacão

--- src/components/Pacientes_cadastro.js
+++ src/components/Pacientes_cadastro.js
@@ -57,65 +57,68 @@ class CadastroPacientes extends Component{
 				<Dashboard />
 				<div className="formulario">
 					<form onSubmit={this.handleSubmit}>
-						<div className="form-inputs">
-							<input 
-							type="number" 
-							name="cpf" 
-							className="" 
-							placeholder="CPF" 
-							value={this.state.cpf} 
-							onChange={this.handleChange} 
-							required
-							/>
+						<div className="formulario_header">
+							<h3>Informações do Paciente</h3>
 						</div>
-						<div className="form-inputs">
-							<input 
-							type="text" 
-							name="nome" 
-							className="" 
-							placeholder="Nome" 
-							value={this.state.nome} 
-							onChange={this.handleChange} 
-							required
-							/>
+						<div className="form-group row">
+							<div className="col-8">
+								<input 
+								type="number" 
+								name="cpf" 
+								className="form-control formulario_input_cpf" 
+								placeholder="CPF" 
+								value={this.state.cpf} 
+								onChange={this.handleChange} 
+								required
+								/>
+							</div>
 						</div>
-						<div className="form-inputs">
-							<input 
-							type="number" 
-							name="idade" 
-							className="" 
-							placeholder="Idade" 
-							value={this.state.idade} 
-							onChange={this.handleChange} 
-							required
-							/>
+						<div className="form-group row">
+							<div className="col-8">
+								<input 
+								type="text" 
+								name="nome" 
+								className="form-control formulario_input" 
+								placeholder="Nome" 
+								value={this.state.nome} 
+								onChange={this.handleChange} 
+								required
+								/>
+							</div>
 						</div>
-						<div className="form-inputs">
-							<select>
-								<option 
-									name="genero" 
-									className="" 
-									onChange={this.handleChange} 
-									value={this.state.genero} 
-									required>Feminino</option>
-								<option 
-									name="genero" 
-									className="" 
-									onChange={this.handleChange} 
-									value={this.state.genero} 
-									required>Masculino</option>
-							</select>
+						<div className="form-group row">
+							<div className="col-8">
+								<input 
+								type="number" 
+								name="idade" 
+								className="form-control formulario_input" 
+								placeholder="Idade" 
+								value={this.state.idade} 
+								onChange={this.handleChange} 
+								required
+								/>
+							</div>
 						</div>
-						<div className="form-inputs">
-							<input 
-							type="text" 
-							name="hip_diag" 
-							className="" 
-							placeholder="Hipótese diagnóstica" 
-							value={this.state.hip_diag} 
-							onChange={this.handleChange} 
-							required
-							/>
+						<div className="form-group row">
+							<div className="col-8 input-group">
+								<select name="genero" value={this.state.genero} onChange={this.handleChange} className="form-control genero_options" required>
+									<option value="feminino">Feminino</option>
+									<option value="masculino">Masculino</option>
+								</select>
+							</div>
+						</div>
+						<div className="form-group row">
+							<div className="col-8">
+								<input 
+								type="text" 
+								name="hip_diag" 
+								className="form-control formulario_input" 
+								placeholder="Hipótese diagnóstica" 
+								value={this.state.hip_diag} 
+								onChange={this.handleChange} 
+								required
+								/>
+							</div>
 						</div>
 						<div className="form-group">
 							<input type="submit" value="Cadastrar" className="btn cadastrar_btn"/>
